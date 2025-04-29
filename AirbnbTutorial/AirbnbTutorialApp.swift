6bbc6d0a580f8f91b3fa5c13bda5087b210a6dca
Arlen.r.pereira <arlen.r.pereira@gmail.com>
2025-04-28 21:24:30 -0300

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Arlen Pereira on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
